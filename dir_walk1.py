5a5ed5704af9b2077269b3a8ebe6fa2382acfaff
dipak dash <ddash@debian-dd-2.debian2.dd>
2017-10-25 18:02:41 +0530

    Newly written dir_walk by me

---
+++ dir_walk1.py
@@ -0,0 +1,32 @@
+import os
+import fnmatch
+import string
+
+def dir_walk(root, pattern='*'):
+
+    result = []
+
+    try:
+        names = os.listdir(root)
+    except os.error as e:
+	raise e
+	return names
+
+    pat_list = string.splitfields(pattern, ';')
+    
+    for name in names:
+        fullname = os.path.normpath(os.path.join(root, name))
+        if os.path.isfile(fullname):
+	    for pat in pat_list:
+                if fnmatch.fnmatch(fullname, pat):
+                    result.append(fullname)
+
+    return result
+
+if __name__ == "__main__":
+    root = "/home/ddash/github/python/"
+    #pattern = "*.py"
+    pattern = "*.xml"
+    names = dir_walk(root, pattern)
+    for name in names:
+        print name 
